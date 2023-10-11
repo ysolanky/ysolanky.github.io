745b970ac25cd2afc487522c3857d99cab935009
Yash Pratap <yash@phidata.com>
2023-10-11 13:23:11 -0400

    email update

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class AppBase extends React.Component {
                 <div>
                     <ul className="c-social">
                         <li>
-                            <a target="_blank"  rel="noreferrer" href="mailto:ysolanky@iu.edu" title="Email">
+                            <a target="_blank"  rel="noreferrer" href="mailto:yashpratapsolanky@gmail.com" title="Email">
                                 <FontAwesomeIcon icon={faEnvelope} color={iconColor}/>
                             </a>
                         </li>
